79d2c15bf753b9b9ee44bb57af60dc263380dd60
mhharktat@gmail.com <mohamed.elkhidir@advisa.se>
2018-11-20 14:33:31 +0100

    fixed sending time and message

--- index.js
+++ index.js
@@ -5,10 +5,14 @@ const scheduler = require('node-schedule');
 const botLogic = require('./src/bot-logic');
 
 const rule = new scheduler.RecurrenceRule();
-rule.dayOfWeek = [new scheduler.Range(1, 5)];
-rule.hour = 11;
+rule.dayOfWeek = [new scheduler.Range(1, 5)]; // from monday to friday
+rule.hour = 10; // 11 am
 rule.minute = 30;
 
 scheduler.scheduleJob(rule, () => {
-  botLogic.sendRemindersToUsers();
-})
+  console.log('schedlued process started');
+  var today = new Date();
+  var yesterday = today.getDay() == 1 ? 'Friday' : 'yesterday';
+
+  botLogic.sendRemindersToUsers(`hej hej, how much of ${yesterday}'s work was sprint realted ?`);
+});
